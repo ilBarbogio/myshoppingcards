1dd427fb87fc5c6dc6b6210f6bd145755dd28e4a
Carminati Alberto <barbogioster@gmail.com>
2026-03-06 22:13:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import { transition as mountMenu, unmount as unmountMenu } from "./scripts/menu.js"
+import { transition as mountAddCard, unmount as unmountAddCard } from "./scripts/addCard.js"
+import QrCodeLib from "../libs/qrcode.js"
+export const QrCode=QrCodeLib
+
+export const outlet=document.getElementById("main-outlet")
+
+export function mount(scene){
+  switch(scene){
+    case "add-card":
+      document.startViewTransition(()=>{
+        unmountMenu()
+        mountAddCard()
+      })
+      break
+    case "menu":
+      document.startViewTransition(()=>{
+        unmountAddCard()
+        mountMenu()
+      })
+    default:
+      break
+  }
+}
+
+const unmount=(scene)=>{
+  switch(scene){
+    case "add-card":
+      unmountAddCard()
+      break
+    case "menu":
+      unmountMenu()
+    default:
+      break
+  }
+}
+
+mount("menu")
